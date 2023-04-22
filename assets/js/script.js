d0c30109a0284149439e37790e3685cdaa403332
Saikou Gassama <gassamasaikou@yahoo.com>
2023-04-22 06:31:15 +0000

    set up structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function runGame() {
+
+}
+function checkAnswert(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incremenScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubtractQuestion (){
+
+}
+function displaymultiplyQuestion(){
+    
+}
