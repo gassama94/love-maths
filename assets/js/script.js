b919ed6c6a5330318816ba4b7e99e52011ffa0f3
Saikou Gassama <gassamasaikou@yahoo.com>
2023-04-23 07:06:17 +0000

    Add favicon, division code game, final ups the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,11 @@
 
 
     }
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if(event.key === "Enter"){
+            checkAnswer();
+        }
+    })
 
     runGame("addition");
 })
@@ -27,6 +32,12 @@
  * and after the user's answer has been processed
   */
 function runGame(gameType) {
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
+
+
+
     // Creates two Random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -38,6 +49,9 @@ function runGame(gameType) {
 
     }else if(gameType === "subtract"){
         displaySubtractQuestion(num1,num2)
+
+    }else if(gameType === "division"){
+        displayDivisionQuestion(num1,num2)
 
     }else {
         alert(`Unknown game type: ${gameType}`);
@@ -82,6 +96,9 @@ function calculateCorrectAnswer(){
     }else if(operator === "-"){
         return[operand1 - operand2, "substract"];
 
+    }else if(operator === "/"){
+        return[operand1 / operand2, "division"];
+
     }else {
         alert(`unimplemented error: ${operator}`);
         throw `unimplemented error: ${operator}. Aborting`;
@@ -122,4 +139,9 @@ function displaymultiplyQuestion(operand1, operand2){
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+}
+function displayDivisionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
