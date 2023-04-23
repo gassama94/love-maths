db8dd942df0a3df85bfcfc57f23c1332254bc8b8
Saikou Gassama <gassamasaikou@yahoo.com>
2023-04-23 06:21:16 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,10 @@ function runGame(gameType) {
 
     if (gameType === "addition"){
         displayAdditionQuestion(num1, num2)
-    }else {
+    }else if(gameType === "multiply"){
+        displaymultiplyQuestion(num1,num2)
+
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting`;
     }
@@ -69,7 +72,11 @@ function calculateCorrectAnswer(){
 
     if(operator === "+"){
         return[operand1 + operand2, "addition"];
-    }else {
+        
+    }else if(operator === "x"){
+        return[operand1 * operand2, "multiply"];
+    }
+    else {
         alert(`unimplemented error: ${operator}`);
         throw `unimplemented error: ${operator}. Aborting`;
     }
@@ -102,6 +109,8 @@ function displayAdditionQuestion(operand1, operand2) {
 function displaySubtractQuestion (){
 
 }
-function displaymultiplyQuestion(){
-
+function displaymultiplyQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
